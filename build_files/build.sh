0750a84d9206560434b380c6febe20179f729584
bri <284789+b-@users.noreply.github.com>
2025-05-16 19:35:42 -0400

    Remove zsh installation from build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,6 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y zsh
 
 python3 /ctx/setup-periphery.py --force --factory
 echo 'writing tmpfiles.d for periphery default config'
