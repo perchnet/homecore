1edafa0f599308e96f4eb9ea31b1e8aa58b55e3e
bri <284789+b-@users.noreply.github.com>
2025-05-16 19:44:14 -0400

    did she really just do that

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,3 +5,5 @@ set -ouex pipefail
 python3 /ctx/setup-periphery.py --force --factory
 echo 'writing tmpfiles.d for periphery default config'
 tee /usr/lib/tmpfiles.d/periphery-default-config.conf <<<'C /etc/komodo/periphery.config.toml 0640 root root - /usr/lib/komodo/periphery.config.toml'
+
+rm -fR /usr/etc
